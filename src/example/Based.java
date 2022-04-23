02ab6db461d4e56e8c1ce4e918f1edbee9fb9252
ExplerHD <73381264+ExplerHD@users.noreply.github.com>
2022-04-23 18:35:50 +0700

    Update and rename ExampleJavaMod.java to Based.java

---
+++ src/example/Based.java
@@ -0,0 +1,28 @@
+package based;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class Based extends Mod{
+
+    public Based(){
+        Log.info("Loaded Based constructor.");
+    }
+
+    @Override
+    public void loadContent(){
+//        new BasedStatus().load();
+//        new BasedIL().load();
+//        new BasedBullets().load();
+//        new BasedUnits().load();
+//        new BasedBlocks().load();
+//        new BasedPlanetary().load();
+//        new BasedTree().load();
+    }
+}
